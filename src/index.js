d7a0d8d8ab110b6ca9178b420dc72f19b13967f5
Hudzenko Valentyn <gfd.lah@gmail.com>
2024-05-14 14:23:29 +0300

    init prac

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {legacy_createStore as createStore, bindActionCreators} from 'redux';
+import reducer from './reducer';
+import * as actions from './actions';
+import Counter from './Counter';
+
+
+const store = createStore(reducer);
+
+const {dispatch, subscribe, getState} = store
+
+const update = () => {
+  
+}
+
+subscribe(update);
+
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// }
+
+const {inc, dec, rnd} = bindActionCreators( actions, dispatch);
+
+
+// document.getElementById('inc').addEventListener('click', inc);
+// document.getElementById('dec').addEventListener('click', dec);
+// document.getElementById('rnd').addEventListener('click', ()=>{
+//   const value = Math.floor(Math.random() * 10);
+//   rnd(value);
+// });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counter
+      counter={getState().value}
+      inc={inc}
+      dec={dec}
+      rnd={()=>{
+        const value = Math.floor(Math.random() * 10);
+        rnd(value);
+      }}
+    />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
